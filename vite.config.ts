a88dfc56edf436cb8f48e5843779d5a658ea52a9
Shawn Roby <shawnroby2005@gmail.com>
2026-04-07 12:29:19 +0530

    Fix plugins for wrangler compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => {
-  const plugins = [
-    tailwindcss(),
-    react(),
-    ...(mode === "development" ? [componentTagger()] : []),
-  ];
-
-  return {
-    server: {
-      host: "::",
-      port: 8080,
+export default defineConfig({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    plugins,
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-  };
+  },
 });
